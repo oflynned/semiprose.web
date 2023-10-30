130623c5118a0fc8050c85bc6db8d20974e87669
Edmond O'Flynn <oflynned@tcd.ie>
2023-10-30 15:34:37 +0100

    feat: create nested menu in navigation bar profile button

--- app/design-system/NavigationBar/NavigationBar.tsx
+++ app/design-system/NavigationBar/NavigationBar.tsx
@@ -1,13 +1,16 @@
-import type { FunctionComponent } from "react";
+import type { ComponentProps, FunctionComponent } from "react";
 import { Button, ProfileButton } from "~/design-system";
 import { Link, useNavigate } from "@remix-run/react";
 import { NavigationItem } from "./NavigationItem";
 import type { User } from "~/types";
+import { useState } from "react";
 
 type Props = {
   page: string;
   user: User;
 };
+
+type MenuOption = ComponentProps<typeof ProfileButton>["options"][number];
 
 type Section = { label: string; id: string };
 
@@ -20,6 +23,28 @@ const section: Section[] = [
 
 export const NavigationBar: FunctionComponent<Props> = ({ page, user }) => {
   const navigate = useNavigate();
+  const [open, setOpen] = useState(false);
+  const [theme, setTheme] = useState<"light" | "dark">("light");
+
+  const options: MenuOption[] = [
+    {
+      label: "Settings",
+      icon: "settings",
+      onClick: () => navigate("/settings"),
+    },
+    theme === "light"
+      ? {
+          label: "Dark mode",
+          icon: "dark_mode",
+          onClick: () => setTheme("dark"),
+        }
+      : {
+          label: "Light mode",
+          icon: "light_mode",
+          onClick: () => setTheme("light"),
+        },
+    { label: "Log out", icon: "logout", onClick: () => navigate("/logout") },
+  ];
 
   return (
     <aside
@@ -28,9 +53,7 @@ export const NavigationBar: FunctionComponent<Props> = ({ page, user }) => {
       }
     >
       <div className={"flex flex-col gap-8"}>
-        <div>
-          <h1 className={"text-4xl font-bold"}>{"Semiprose"}</h1>
-        </div>
+        <h1 className={"text-4xl font-bold"}>{"Semiprose"}</h1>
         <div className={"flex justify-center"}>
           <Button
             label={"Compose"}
@@ -46,9 +69,12 @@ export const NavigationBar: FunctionComponent<Props> = ({ page, user }) => {
           ))}
         </div>
       </div>
-      <div>
-        <ProfileButton {...user} />
-      </div>
+      <ProfileButton
+        {...user}
+        options={options}
+        open={open}
+        onClick={() => setOpen((value) => !value)}
+      />
     </aside>
   );
 };
--- app/design-system/ProfileButton/ProfileButton.tsx
+++ app/design-system/ProfileButton/ProfileButton.tsx
@@ -1,26 +1,63 @@
 import type { FunctionComponent } from "react";
 import { Avatar } from "~/design-system";
 import type { User } from "~/types";
+import classNames from "classnames";
 
 type Props = {
+  open: boolean;
+  options: Option[];
   onClick?: () => void;
 } & Pick<User, "initials" | "username">;
 
+type Option = {
+  label: string;
+  icon: string;
+  onClick?: () => void;
+};
+
 export const ProfileButton: FunctionComponent<Props> = ({
+  options,
+  open,
   initials,
   username,
   onClick,
 }) => {
   return (
     <div
-      className={
-        "flex items-center gap-2 rounded-xl hover:bg-gray-100 active:bg-gray-200 p-2 clickable"
-      }
-      onClick={onClick}
+      className={classNames([
+        "rounded-2xl overflow-hidden border-2 border-transparent",
+        { "border-gray-100 bg-gray-50": open },
+      ])}
     >
-      <Avatar initials={initials} />
-      <div className={"flex items-center"}>
-        <p>{username}</p>
+      <div
+        className={classNames([
+          "flex flex-col align-middle divide-y",
+          { hidden: !open },
+        ])}
+      >
+        {options.map(({ label, icon, onClick }) => (
+          <div
+            onClick={() => onClick?.()}
+            className={
+              "flex flex-row gap-2 p-4 hover:bg-gray-100 active:bg-gray-200 clickable"
+            }
+          >
+            <span className={"material-symbols-outlined"}>{icon}</span>
+            <p>{label}</p>
+          </div>
+        ))}
+      </div>
+      <div
+        className={classNames([
+          "flex items-center gap-2 rounded-xl hover:bg-gray-100 active:bg-gray-200 p-2 clickable",
+          { "bg-gray-200": open },
+        ])}
+        onClick={onClick}
+      >
+        <Avatar initials={initials} />
+        <div className={"flex items-center"}>
+          <p>{username}</p>
+        </div>
       </div>
     </div>
   );
