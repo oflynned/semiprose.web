5b91f61746358b9f66aeaf937c5963d69cc77ade
Edmond O'Flynn <oflynned@tcd.ie>
2023-10-30 15:52:02 +0100

    feat: add example settings

--- app/routes/settings.tsx
+++ app/routes/settings.tsx
@@ -5,6 +5,14 @@ export default function Settings() {
     <Layout>
       <div className={"flex flex-col gap-4"}>
         <h3 className={"font-bold text-4xl"}>{"Settings"}</h3>
+        <div>
+          <h4 className={"font-medium text-xl"}>{"Privacy"}</h4>
+          <p>{"Who can see your stories"}</p>
+        </div>
+        <div>
+          <h4 className={"font-medium text-xl"}>{"Danger Zone"}</h4>
+          <p>{"Delete your account"}</p>
+        </div>
       </div>
     </Layout>
   );
