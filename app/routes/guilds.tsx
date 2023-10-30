d9ca2b32689b00eaea5a032fd5b1755e420723d1
Edmond O'Flynn <oflynned@tcd.ie>
2023-10-30 15:33:47 +0100

    feat: show a title and some content on guilds and notifications

--- app/routes/guilds.tsx
+++ app/routes/guilds.tsx
@@ -3,7 +3,9 @@ import { Layout } from "~/design-system";
 export default function Guilds() {
   return (
     <Layout>
-      <p>{"Guilds"}</p>
+      <div className={"flex flex-col gap-4"}>
+        <h3 className={"font-bold text-4xl"}>{"Guilds"}</h3>
+      </div>
     </Layout>
   );
 }
--- app/routes/notifications.tsx
+++ app/routes/notifications.tsx
@@ -3,7 +3,10 @@ import { Layout } from "~/design-system";
 export default function Notifications() {
   return (
     <Layout>
-      <p>{"Notifications"}</p>
+      <div className={"flex flex-col gap-4"}>
+        <h3 className={"font-bold text-4xl"}>{"Notifications"}</h3>
+        <p>{"Hooray! You're all caught up."}</p>
+      </div>
     </Layout>
   );
 }
