bbc3fddae7fbd0800ea13521b20f68fb0d2d2f4c
Edmond O'Flynn <oflynned@tcd.ie>
2023-10-31 18:31:26 +0100

    feat: load story on detail page with loader

--- app/routes/stories.$slug.tsx
+++ app/routes/stories.$slug.tsx
@@ -1,10 +1,19 @@
 import { Layout, Story } from "~/design-system";
-import { story } from "~/constants";
+import { useLoaderData } from "@remix-run/react";
+import type { LoaderFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { pastStories } from "~/constants";
+
+export const loader: LoaderFunction = async ({ params }) => {
+  return json({
+    story: pastStories.filter((story) => story.id === params.slug)[0],
+  });
+};
 
 export default function StoryDetail() {
+  const { story } = useLoaderData<typeof loader>();
+
   return (
-    <Layout>
-      <Story {...story} />
-    </Layout>
+    <Layout>{story ? <Story {...story} /> : <p>{"Story not found"}</p>}</Layout>
   );
 }
