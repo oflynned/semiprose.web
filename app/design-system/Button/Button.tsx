f600987da5fa9c60ad3e1e6e1314707d56812709
Edmond O'Flynn <oflynned@tcd.ie>
2023-11-01 12:50:14 +0100

    feat: add button variants

--- app/design-system/Button/Button.tsx
+++ app/design-system/Button/Button.tsx
@@ -1,15 +1,37 @@
 import type { ButtonHTMLAttributes, FunctionComponent } from "react";
 import classNames from "classnames";
+import { Spinner } from "~/design-system";
+
+type Variant = "primary" | "outlined" | "text";
+
+type Visibility = "visible" | "invisible" | "gone";
 
 type Props = {
   label: string;
-  visibility?: "visible" | "invisible" | "gone";
+  loading?: boolean;
+  variant?: Variant;
+  visibility?: Visibility;
 } & ButtonHTMLAttributes<HTMLButtonElement>;
+
+const variants: Record<Variant, string> = {
+  primary: "bg-black text-white",
+  outlined: "bg-white text-black border border-black",
+  text: "bg-white text-black",
+};
+
+const interactivity: Record<Variant, string> = {
+  primary: "hover:bg-gray-800 active:bg-gray-600 hover:shadow active:shadow-md",
+  outlined:
+    "hover:bg-gray-100 active:bg-gray-200 hover:shadow active:shadow-md",
+  text: "hover:bg-gray-100 active:bg-gray-200",
+};
 
 export const Button: FunctionComponent<Props> = ({
   onClick,
   label,
-  disabled,
+  variant = "primary",
+  disabled = false,
+  loading = false,
   visibility = "visible",
   ...props
 }) => {
@@ -17,16 +39,21 @@ export const Button: FunctionComponent<Props> = ({
     <button
       {...props}
       className={classNames([
-        "bg-black text-white rounded-xl py-2 px-8 hover:shadow-lg clickable",
-        disabled
-          ? "opacity-50 cursor-not-allowed"
-          : "opacity-100 cursor-pointer",
+        variants[variant],
+        "rounded-lg py-2 px-8",
+        { [`${interactivity[variant]} clickable`]: !disabled && !loading },
+        { "opacity-50 cursor-not-allowed": disabled },
+        { "cursor-not-allowed": loading },
         visibility,
       ])}
       disabled={disabled}
       onClick={onClick}
     >
-      <span className={"uppercase font-bold"}>{label}</span>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <span className={"uppercase font-bold"}>{label}</span>
+      )}
     </button>
   );
 };
