641f552f3de78a3ad2b344c0d2e4fcd0fd74542e
Edmond O'Flynn <oflynned@tcd.ie>
2023-11-02 13:05:16 +0100

    chore: revert loading order

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -7,7 +7,6 @@
 import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
-import { ThemeProvider } from "~/context";
 
 startTransition(() => {
   hydrateRoot(
