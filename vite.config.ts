5f02454f78a22a33ec7891357cc67dce141719ce
Edmond O'Flynn <oflynned@tcd.ie>
2023-11-02 10:33:35 +0100

    chore(build): use vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
-
-import react from "@vitejs/plugin-react";
+import { unstable_vitePlugin as remix } from "@remix-run/dev";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [remix()],
 });
